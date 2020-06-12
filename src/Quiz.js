2c47c4865df9176efa0798840a39b2ce13da63fc
mano9696 <66700302+mano9696@users.noreply.github.com>
2020-06-12 23:53:04 +0100

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,7 +14,7 @@ class Quiz extends Component {
             return { quiz_position: state.quiz_position +1 }
         })
         }
-    }
+    
 
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
